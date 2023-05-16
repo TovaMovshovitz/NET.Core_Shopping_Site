53fd6d5638ad624b5576cf2b1b3a4452f41e5dcc
tova <36213354335@mby.co.il>
2023-05-16 14:59:10 +0300

    validations

--- DTO/UserDto.cs
+++ DTO/UserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,8 @@ namespace DTO
         public string FirstName { get; set; } = null!;
 
         public string? LastName { get; set; }
-
+        
+        [EmailAddress]
         public string Email { get; set; } = null!;
 
     }
--- entities/User.cs
+++ entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace entities;
 
@@ -10,9 +11,11 @@ public partial class User
     public string? FirstName { get; set; } = null!;
 
     public string? LastName { get; set; }
-
+    
+    [EmailAddress]
     public string Email { get; set; } = null!;
-
+    
+    [StringLength(20, ErrorMessage = "string length must be between 4-20", MinimumLength = 4)]
     public string Password { get; set; } = null!;
 
     public virtual ICollection<Order> Orders { get; } = new List<Order>();
